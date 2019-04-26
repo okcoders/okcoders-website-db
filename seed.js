521ca94634874a2257a78be10419b0b940641323
Terry Brown <ptbrown514@gmail.com>
2019-04-25 20:21:01 -0500

    initial commit, added some fields and alumni

--- seed.js
+++ seed.js
@@ -1,5 +1,39 @@
 db.alumnis.insertOne({
+  card: "Zach Mays",
   firstName: "Zach",
   lastName: "Mays",
+  avatar: "https://avatars2.githubusercontent.com/u/4370615?s=400&v=4",
   age: 29
-})
+});
+
+db.alumnis.insertOne({
+  card: "Terry Brown",
+  firstName: "Terry",
+  lastName: "Brown",
+  avatar: "https://avatars1.githubusercontent.com/u/42389818?s=400&v=4",
+  age: 30
+});
+
+db.alumnis.insertOne({
+  card: "Joseph Okpara",
+  firstName: "Joseph",
+  lastName: "Okpara",
+  avatar: "https://avatars1.githubusercontent.com/u/42412163?s=400&v=4",
+  age: 30
+});
+
+db.alumnis.insertOne({
+  card: "Brayden Robbins",
+  firstName: "Brayden",
+  lastName: "Robbins",
+  avatar: "https://avatars2.githubusercontent.com/u/42324388?s=400&v=4",
+  age: 24
+});
+
+db.alumnis.insertOne({
+  card: "Susan Moring",
+  firstName: "Susan",
+  lastName: "Moring",
+  avatar: "https://avatars0.githubusercontent.com/u/26634819?s=400&v=4",
+  age: 29
+});
