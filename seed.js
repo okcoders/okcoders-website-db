3cfe9296fc3374c9668ecd9b29bbedbe211d010f
Terry Brown <ptbrown514@gmail.com>
2019-04-28 18:35:37 -0500

    added linkedin and github links

--- seed.js
+++ seed.js
@@ -1,39 +1,44 @@
 db.alumnis.insertOne({
-  card: "Zach Mays",
   firstName: "Zach",
   lastName: "Mays",
   avatar: "https://avatars2.githubusercontent.com/u/4370615?s=400&v=4",
-  age: 29
+  age: 29,
+  linkedin: "https://www.linkedin.com/in/zmays/",
+  github: "https://github.com/zmays"
 });
 
 db.alumnis.insertOne({
-  card: "Terry Brown",
   firstName: "Terry",
   lastName: "Brown",
   avatar: "https://avatars1.githubusercontent.com/u/42389818?s=400&v=4",
-  age: 30
+  age: 30,
+  linkedin: "https://www.linkedin.com/in/terry-brown-9aa1b8119/",
+  github: "https://github.com/ptbrown4"
 });
 
 db.alumnis.insertOne({
-  card: "Joseph Okpara",
   firstName: "Joseph",
   lastName: "Okpara",
   avatar: "https://avatars1.githubusercontent.com/u/42412163?s=400&v=4",
-  age: 30
+  age: 30,
+  linkedin: "https://www.linkedin.com/in/joseph-okpara-0b8a6429/",
+  github: "https://github.com/ojo88"
 });
 
 db.alumnis.insertOne({
-  card: "Brayden Robbins",
   firstName: "Brayden",
   lastName: "Robbins",
   avatar: "https://avatars2.githubusercontent.com/u/42324388?s=400&v=4",
-  age: 24
+  age: 20,
+  linkedin: "https://www.linkedin.com/",
+  github: "https://github.com/braydenrobbins"
 });
 
 db.alumnis.insertOne({
-  card: "Susan Moring",
   firstName: "Susan",
   lastName: "Moring",
   avatar: "https://avatars0.githubusercontent.com/u/26634819?s=400&v=4",
-  age: 29
+  age: 29,
+  linkedin: "https://www.linkedin.com/in/susanmoring/",
+  github: "https://github.com/smoring"
 });
