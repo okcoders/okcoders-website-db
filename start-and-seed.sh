90c7bf2a772fb659f7cf2ba0e8c4a4cc839a869d
Zach Mays <zsmays@gmail.com>
2019-06-29 09:42:48 -0500

    dont run docker command in foreground

--- start-and-seed.sh
+++ start-and-seed.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+docker stop website-db
 docker run --rm -d -p 27017:27017 --name website-db mongo
 
 # wait for server to start up
 sleep 5
 
-docker run -it --rm --network host -v `pwd`/seed.js:/seed.js mongo mongo okcoders /seed.js
+docker run --rm --network host -v `pwd`/seed.js:/seed.js mongo mongo okcoders /seed.js
